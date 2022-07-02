c14a2e92a0ea643626b2174aa6fd3d15e513f07c
jackiejing <jackie.jingg1125@gmail.com>
2022-07-01 20:39:46 -0400

    Create MyList.cs

---
+++ Assignment4/Assignment4/MyList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class MyList<T>
+    {
+        List<T> List = new List<T>();
+        public void Add(T element)
+        {
+            List.Add(element);
+        }
+        public T Remove(int index)
+        {
+            return List.RemoveAt(index);
+        }
+        public bool Contains(T element)
+        {
+            return List.Contains(element);
+        }
+        public void Clear()
+        {
+            List.Clear();
+        }
+        public void InsertAt(T element, int index)
+        {
+            List.Insert(index, element);
+        }
+        public void DeleteAt(int index)
+        {
+            List.RemoveAt(index);
+        }
+        public T Find(int index)
+        {
+            T element = List[index];
+            return element;
+        }
+    }
+}
