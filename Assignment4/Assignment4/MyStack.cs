2c2d59316db05608f21d57c3682a4a4b2e9b6161
jackiejing <jackie.jingg1125@gmail.com>
2022-07-01 20:39:48 -0400

    Create MyStack.cs

---
+++ Assignment4/Assignment4/MyStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class MyStack<T>
+    {
+        Stack<T> stack = new Stack<T>();
+        public int Count()
+        {
+            return stack.Count;
+        }
+        public T Pop()
+        {
+            return stack.Pop();
+        }
+        public void push(T x)
+        {
+            stack.Push(x);
+        }
+    }
+}
