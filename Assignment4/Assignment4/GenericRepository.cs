da9882f762cc3a1c39acbb955fec9c9cbbcbf50d
jackiejing <jackie.jingg1125@gmail.com>
2022-07-01 20:39:42 -0400

    Create GenericRepository.cs

---
+++ Assignment4/Assignment4/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        List<T> List = new List<T>();
+        public void Add(T item)
+        {
+            List.Add(item);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return List;
+        }
+
+        public T GetById(int id)
+        {
+            return List.Find(id);
+        }
+
+        public void Remove(T item)
+        {
+            List.Remove(item);
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
